9fb0ab24e923e66039304c1b5b7a84ec14a1ed86
Thorsten Lorenz <thlorenz@gmx.de>
2013-03-14 09:03:46 -0400

    transform adds sourcemap with inlined original source

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 var coffee = require('coffee-script');
 var through = require('through');
+var convert = require('convert-source-map');
+
+function compile(file, data) {
+    var compiled = coffee.compile(data, { sourceMap: true, filename: file });
+    var comment = convert
+        .fromJSON(compiled.v3SourceMap)
+        .setProperty('sourcesContent', [ data ])
+        .toComment();
+
+    return compiled.js + '\n' + comment;
+}
 
 module.exports = function (file) {
     if (!/\.coffee$/.test(file)) return through();
@@ -9,7 +20,7 @@ module.exports = function (file) {
     
     function write (buf) { data += buf }
     function end () {
-        this.queue(coffee.compile(data));
+        this.queue(compile(file, data));
         this.queue(null);
     }
 };
