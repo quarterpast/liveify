8bd017b27b17ab91254723f3082a727b62958e1d
Matt Brennan <mattyb1000@gmail.com>
2013-03-24 08:32:02 +0000

    does livescript

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
-var coffee = require('coffee-script');
+var LiveScript = require('LiveScript');
 var through = require('through');
-var convert = require('convert-source-map');
-
-function compile(file, data) {
-    var compiled = coffee.compile(data, { sourceMap: true, generatedFile: file, inline: true });
-    var comment = convert
-        .fromJSON(compiled.v3SourceMap)
-        .setProperty('sources', [ file ])
-        .toComment();
-
-    return compiled.js + '\n' + comment;
-}
-
 module.exports = function (file) {
-    if (!/\.coffee$/.test(file)) return through();
+    if (!/\.ls/.test(file)) return through();
     
     var data = '';
     return through(write, end);
     
     function write (buf) { data += buf }
     function end () {
-        this.queue(compile(file, data));
+        this.queue(LiveScript.compile(data,{filename:file,bare:true}));
         this.queue(null);
     }
 };
